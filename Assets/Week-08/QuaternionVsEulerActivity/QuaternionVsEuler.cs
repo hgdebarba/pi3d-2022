115f235098f077a2829d260d7fc8599267642e5e
hgdebarba <hgdebarba@gmail.com>
2022-10-27 23:51:11 +0200

    week 8 - completed activities

--- Assets/Week-08/QuaternionVsEulerActivity/QuaternionVsEuler.cs
+++ Assets/Week-08/QuaternionVsEulerActivity/QuaternionVsEuler.cs
@@ -11,6 +11,17 @@ namespace Week08
 
         void Update()
         {
+            float t = Time.time % duration / duration;
+
+            Quaternion qFrom = startRot.rotation;
+            Quaternion qTo = endRot.rotation;
+            Quaternion qt = Quaternion.Slerp(qFrom, qTo, t);
+
+            Vector3 eFrom = startRot.eulerAngles;
+            Vector3 eTo = endRot.eulerAngles;
+            Vector3 et = Vector3.Lerp(eFrom, eTo, t);
+
+            transform.rotation = useQuaternion ? qt : Quaternion.Euler(et);
 
         }
     }

90e1f1011e997c7b49263e942348c3f460fb2521
hgdebarba <hgdebarba@gmail.com>
2023-11-08 10:17:26 +0100

    removed 3D transformation solution plus Unity upgrade

--- Assets/Week-08/Matrix3DExercise/TransformMatrix.cs
+++ Assets/Week-08/Matrix3DExercise/TransformMatrix.cs
@@ -30,48 +30,31 @@ namespace Week08
 
             // TODO create the translation matrix
             Matrix4x4 T = Matrix4x4.identity;
-            T.m03 = position.x;
-            T.m13 = position.y;
-            T.m23 = position.z;
 
             // TODO create the scale matrix
             Matrix4x4 S = Matrix4x4.identity;
-            S.m00 = scale.x;
-            S.m11 = scale.y;
-            S.m22 = scale.z;
-
 
             // TODO create three rotation matrices, one per rotation axis/euler angle
             Matrix4x4 RX = Matrix4x4.identity;
-            float angleRadX = Mathf.Deg2Rad * rotation.x;
-            RX.m11 = Mathf.Cos(angleRadX);
-            RX.m21 = Mathf.Sin(angleRadX);
-            RX.m12 = -Mathf.Sin(angleRadX);
-            RX.m22 = Mathf.Cos(angleRadX);
             Matrix4x4 RY = Matrix4x4.identity;
-            float angleRadY = Mathf.Deg2Rad * rotation.y;
-            RY.m00 = Mathf.Cos(angleRadY);
-            RY.m02 = Mathf.Sin(angleRadY);
-            RY.m20 = -Mathf.Sin(angleRadY);
-            RY.m22 = Mathf.Cos(angleRadY);
             Matrix4x4 RZ = Matrix4x4.identity;
             // rotation.z/RZ is given as an example
-            float angleRad = Mathf.Deg2Rad * rotation.z;
-            RZ.m00 =Mathf.Cos(angleRad);
-            RZ.m10 = Mathf.Sin(angleRad);
-            RZ.m01 = -Mathf.Sin(angleRad);
-            RZ.m11 = Mathf.Cos(angleRad);
+            float angleRadZ = Mathf.Deg2Rad * rotation.z;
+            RZ.m00 =Mathf.Cos(angleRadZ);
+            RZ.m10 = Mathf.Sin(angleRadZ);
+            RZ.m01 = -Mathf.Sin(angleRadZ);
+            RZ.m11 = Mathf.Cos(angleRadZ);
 
             // TODO concatenate the three matrices together
             // remember that, when using euler angles, rotation order matters!
             // by default, Unity implements the order Rotation Z -> then X -> then Y
             // notice that, since we use column vectors, transformations are applied from RIGHT TO LEFT
-            Matrix4x4 R = RY * RX* RZ; // R = multiply RX, RY and RZ in the CORRECT ORDER
+            Matrix4x4 R = RZ; // R = multiply RX, RY and RZ in the CORRECT ORDER
 
             // TODO concatenate the transformations into a single matrix
             // we first Scale -> then Rotate -> and then Translate
             // notice that, since we use column vectors, transformations are applied from RIGHT TO LEFT
-            TRSMatrix = T * R * S; // TRSMatrix = multiply translation, scale and rotation in the CORRECT ORDER
+            TRSMatrix = R; // TRSMatrix = multiply translation, scale and rotation in the CORRECT ORDER
 
 
             return TRSMatrix;
